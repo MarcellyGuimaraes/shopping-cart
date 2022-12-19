a4df49e56156ef1fa7accdf8e5e887f3cbe6acbe
IsisG13 <isissouzaguimaraes@gmail.com>
2022-12-19 18:50:00 -0300

    arrumando uma parte do css

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -6,25 +6,32 @@ const ProductCard = ({ product }) => {
   const { addToCart } = useContext(CartContext)
 
   return (
-    <div className="productCard__wrapper">
-      <div>
-        <img className="productCard__img" src={product.image} alt="" />
-        <h4>{product.name}</h4>
-        <div className="ProductCard__price">
-          <h5>{product.price}</h5>
+    <div class="w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <a href="#">
+        <img class="p-8 rounded-t-lg" src={product.image} alt="product image" />
+      </a>
+
+      <div className="px-5 pb-5">
+        <a href="#">
+          <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+            {product.name}
+          </h5>
+        </a>
+        <div className="flex items-center mt-2.5 mb-5">
+          <Rating text={product.rating} />
         </div>
-        <div className="ProductCard__Rateing">
-          <Rating
-            value={product.rating}
-            text={`${product.numReviews} reviews`}
-          />
+        <div className="flex items-center justify-between">
+          <span className="text-3xl font-bold text-gray-900 dark:text-white">
+            {product.price}
+          </span>
+          <a
+            href="#"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            onClick={() => addToCart(product)}
+          >
+            Add to cart
+          </a>
         </div>
-        <button
-          className="ProductCard__button"
-          onClick={() => addToCart(product)}
-        >
-          Add to basket
-        </button>
       </div>
     </div>
   )
--- src/components/Rating/index.jsx
+++ src/components/Rating/index.jsx
@@ -1,25 +1,24 @@
 import React from 'react'
 
-const Rating = ({ value, text, color }) => {
+const Rating = ({ text }) => {
   return (
-    <div>
-      {[1, 2, 3, 4, 5].map((rate) => (
-        <span key={rate}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </span>
+    <div className="flex items-center mt-2.5 mb-5">
+      {[1, 2, 3, 4, 5].map((index) => (
+        <svg
+          key={index}
+          aria-hidden="true"
+          className="w-5 h-5 text-yellow-300"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <title>First star</title>
+          <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+        </svg>
       ))}
-      <span>{text && text}</span>
+      <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
+        {text && text}
+      </span>
     </div>
   )
 }
