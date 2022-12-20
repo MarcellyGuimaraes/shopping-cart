024cd8c5c71ff8587c8e4fd786534f49f32bd075
IsisG13 <isissouzaguimaraes@gmail.com>
2022-12-20 11:21:38 -0300

    Arrumando detalhes no css do cart

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -9,31 +9,48 @@ const Cart = () => {
     <>
       {showCart && (
         <div className="fixed top-20 right-8 rounded-sm bg-white border border-gray-400 p-1 z-10">
-          <div>
-            <span onClick={showHideCart}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
+          <div className="flex items-start justify-between">
+            <h2
+              className="text-lg font-medium text-gray-900"
+              id="slide-over-title"
+            >
+              Carrinho
+            </h2>
+            <div className="ml-3 flex h-7 items-center">
+              <button
+                type="button"
+                onClick={showHideCart}
+                className="-m-2 p-2 text-gray-400 hover:text-gray-500"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-            </span>
+                <span className="sr-only">Fechar</span>
+
+                <svg
+                  className="h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
           </div>
-          <div className="h-max max-h-[50vh] overflow-y-auto">
+          <div className="h-max max-h-[50vh] w-72 overflow-y-auto">
             {cartItems.length === 0 ? (
-              <h4>Cart is Empty</h4>
+              <h4 className="py-5 text-lg font-bold text-black">
+                O carrinho está vazio
+              </h4>
             ) : (
               <ul>
-                {cartItems.map((item) => (
-                  <CartItem key={item._id} item={item} />
+                {cartItems.map((item, index) => (
+                  <CartItem key={item.index} item={item} />
                 ))}
               </ul>
             )}
@@ -41,8 +58,11 @@ const Cart = () => {
           <div className="mt-2 flex justify-between">
             <div>Cart Total</div>
             <div></div>
-            <div style={{ marginLeft: 5 }}>
-              {cartItems.reduce((amount, item) => item.price + amount, 0)}
+            <div className="text-green-600">
+              R${' '}
+              {cartItems
+                .reduce((amount, item) => item.price + amount, 0)
+                .toFixed(2)}
             </div>
           </div>
         </div>
--- src/components/CartItem/index.jsx
+++ src/components/CartItem/index.jsx
@@ -4,17 +4,36 @@ import CartContext from '../../context/Cart/CartContext'
 const CartItem = ({ item }) => {
   const { removeItem } = useContext(CartContext)
   return (
-    <li className="mt-1 flex h-24 list-none items-center gap-1 border-b border-gray-400 px-1">
-      <img className="h-full w-[100px] object-cover" src={item.image} alt="" />
-      <div>
-        {item.name} {item.price}
+    <li className="flex py-6">
+      <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+        <img
+          src={item.image}
+          alt="Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt."
+          className="h-full w-full object-cover object-center"
+        />
       </div>
-      <button
-        className="ml-auto px-2 py-4 border-none cursor-pointer rounded-md"
-        onClick={() => removeItem(item._id)}
-      >
-        Remove
-      </button>
+
+      <div className="ml-4 flex flex-1 flex-col">
+        <div>
+          <div className="flex justify-between text-base font-medium text-gray-900">
+            <h3>
+              <a href="#">{item.name}</a>
+            </h3>
+          </div>
+          <p className="mt-1 text-sm text-gray-500">R${item.price}</p>
+        </div>
+        <div className="flex flex-1 items-end justify-between text-sm">
+          <div className="flex">
+            <button
+              type="button"
+              className="font-medium text-indigo-600 hover:text-indigo-500"
+              onClick={() => removeItem(item._id)}
+            >
+              Remove
+            </button>
+          </div>
+        </div>
+      </div>
     </li>
   )
 }
