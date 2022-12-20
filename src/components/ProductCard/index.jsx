3f662d9efd259ed0c0adbfa543ddf98fc46a8276
IsisG13 <isissouzaguimaraes@gmail.com>
2022-12-20 10:46:35 -0300

    arrumando css do Product Card

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -6,31 +6,39 @@ const ProductCard = ({ product }) => {
   const { addToCart } = useContext(CartContext)
 
   return (
-    <div class="w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-      <a href="#">
-        <img class="p-8 rounded-t-lg" src={product.image} alt="product image" />
-      </a>
-
-      <div className="px-5 pb-5">
-        <a href="#">
-          <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-            {product.name}
-          </h5>
-        </a>
-        <div className="flex items-center mt-2.5 mb-5">
-          <Rating text={product.rating} />
-        </div>
-        <div className="flex items-center justify-between">
-          <span className="text-3xl font-bold text-gray-900 dark:text-white">
-            {product.price}
-          </span>
-          <a
-            href="#"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            onClick={() => addToCart(product)}
-          >
-            Add to cart
-          </a>
+    <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:shadow-xl">
+      <img
+        src={product.image}
+        alt="Product"
+        className="h-80 w-72 object-cover rounded-t-xl"
+      />
+      <div className="px-4 py-3 w-72">
+        <span className="text-gray-400 mr-3 uppercase text-xs">
+          <Rating text={`${product.rating} Estrelas`} />
+        </span>
+        <p className="text-lg font-bold text-black truncate block capitalize">
+          {product.name}
+        </p>
+        <div className="flex items-center">
+          <p className="text-lg font-semibold text-black cursor-auto my-3">
+            R$ {product.price}
+          </p>
+          <div className="ml-auto" onClick={() => addToCart(product)}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="currentColor"
+              className="bi bi-bag-plus"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
+              />
+              <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+            </svg>
+          </div>
         </div>
       </div>
     </div>
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -4,7 +4,7 @@ import ProductCard from '../../components/ProductCard'
 
 const Home = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
+    <div className="mx-auto mt-32 mb-5 grid w-fit grid-cols-1 justify-center justify-items-center gap-y-20 gap-x-14 md:grid-cols-2 lg:grid-cols-4">
       {products.map((product, index) => (
         <ProductCard key={index} product={product} />
       ))}
